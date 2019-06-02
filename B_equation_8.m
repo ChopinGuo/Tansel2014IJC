7c9f6bc6f68ef95dc002dd2ab8c54796a66c0661
ChopinGuo <guoshaopan@pku.edu.cn>
2019-06-02 13:02:44 +0800

    02

--- B_equation_8.m
+++ B_equation_8.m
@@ -13,7 +13,7 @@ alpha3 = (1-sech(xi25*x).^2).^theta3;
 alpha4 = (1-sech(xi25*x).^2).^theta4;
 alpha5 = (1-sech(xi25*x).^2).^theta5;
 
-figure(1),plot(x,alpha1,'r-'),legend('Equation (8) (theta)');
+figure(1),plot(x,alpha1,'r-')
 hold on;
 plot(x,alpha2,'y-')
 hold on;
@@ -23,6 +23,7 @@ plot(x,alpha4,'b-')
 hold on;
 plot(x,alpha5,'k-')
 grid on;
+legend('(xi=25, theta=1)','(xi=25, theta=2)','(xi=25, theta=3)','(xi=25, theta=4)','(xi=25, theta=5)');
 
 theta=1;
 xi1=1;
@@ -37,8 +38,7 @@ alpha3 = (1-sech(xi25*x).^2).^theta1;
 alpha4 = (1-sech(xi35*x).^2).^theta1;
 alpha5 = (1-sech(xi50*x).^2).^theta1;
 
-figure(2),plot(x,alpha1,'g-'),legend('Equation (8) (right)');
-hold on;
+figure(2),plot(x,alpha1,'g-')
 plot(x,alpha2,'y-')
 hold on;
 plot(x,alpha3,'r-')
@@ -46,4 +46,9 @@ hold on;
 plot(x,alpha4,'b-')
 hold on;
 plot(x,alpha5,'k-')
+grid on;
+legend('(theta=1,xi=1)','(theta=1,xi=10)','(theta=1,xi=25)','(theta=1,xi=35)','(theta=1,xi=50)');
+
+alpha1000 = (1-sech(1000*x).^2).^1000;
+figure(3),plot(x,alpha1000,'r-'),legend('(theta=1000, xi=1000)');
 grid on;
